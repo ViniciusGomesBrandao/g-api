df040a5fc8a6959421bd30e367102901ebff79a4
Leandro Araujo <leandro.bnk@gmail.com>
2023-07-23 12:24:40 -0300

    install prisma and lib bigdatacorp

--- libs/bigdatacorp/src/bigdatacorp.module.ts
+++ libs/bigdatacorp/src/bigdatacorp.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BigdatacorpService } from './bigdatacorp.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `${__dirname}/../../../../libs/bigdatacorp/.bigdatacorp.env`,
+    }),
+  ],
   providers: [BigdatacorpService],
   exports: [BigdatacorpService],
 })
--- libs/bigdatacorp/src/bigdatacorp.service.ts
+++ libs/bigdatacorp/src/bigdatacorp.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
+interface IOutput {
+  success: boolean;
+  message: string;
+  data?: any;
+}
+
 @Injectable()
-export class BigdatacorpService {}
+export class BigdatacorpService {
+  private _host: string = `${process.env._BIGDATACORP_ENVIRONMENT_URL}`;
+  private _username: string = `${process.env._BIGDATACORP_ENVIRONMENT_USERNAME}`;
+  private _password: string = `${process.env._BIGDATACORP_ENVIRONMENT_PASSWORD}`;
+  private _expiration_minute: number = 1440; //24H
+  private _tz: string = 'America/Sao_Paulo';
+  private _origin: string = 'bigdatacorp';
+}
